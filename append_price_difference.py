f5cc6cafba0c1f944a779f5e89c1c3157e777802
Arch7399 <raymond17399@gmail.com>
2024-11-02 14:26:22 +0530

    add better dataframe naming

--- append_price_difference.py
+++ append_price_difference.py
@@ -4,29 +4,27 @@ from price_difference import calculate_latest_price_difference
 import glob
 
 
-def append_price_changes(df2):
+def append_price_changes(Crypto_API_df):
 
     csv_files = {
-        "Bullish",
-        "HighRSI",
-        "LowRSI",
-        "PriceSpikes",
-        "LowLiquidity",
-        "VolumeSurges",
         "PromisingCurrencies",
     }
 
     # Loop through each file and calculate the price differences
     for csv_file in csv_files:
 
-        output_file = rf"C:/Users/{os.getenv('USER')}/Desktop/price_difference_dump/Price_Diff_{csv_file}.csv"
+        price_diff_output_file = rf"C:/Users/{os.getenv('USER')}/Desktop/price_difference_dump/Price_Diff_{csv_file}.csv"
 
         # Read the CSV file
-        df = pd.read_csv(
+        price_diff_df = pd.read_csv(
             rf"C:/Users/{os.getenv('USER')}/Desktop/Analysis/{csv_file}.csv"
         )
 
         # Calculate and append price differences between consecutive timestamps
-        calculate_latest_price_difference(df, output_file, df2)
+        calculate_latest_price_difference(
+            price_diff_df, price_diff_output_file, Crypto_API_df
+        )
 
-        print(f"Processed file: {csv_file}, differences saved to: {output_file}")
+        print(
+            f"Processed file: {csv_file}, differences saved to: {price_diff_output_file}"
+        )
